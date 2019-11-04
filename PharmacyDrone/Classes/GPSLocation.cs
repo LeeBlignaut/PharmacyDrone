d29b8aa062d070461c5f8b42f5ad6640491e2b82
Damian Smallwood <wazsmallwood@gmail.com>
2019-11-04 19:36:10 +0200

    Added Gps Location Class with GetGpsLocation and GetDistance Methods

---
+++ PharmacyDrone/Classes/GPSLocation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyDrone.Classes
+{
+    class GPSLocation
+    {
+        GeoCoordinate gpsLocation;
+
+        public GeoCoordinate GpsLocation { get => gpsLocation; set => gpsLocation = value; }
+
+        public GPSLocation()
+        {
+
+        }
+        public double GetDistance()
+        {
+            GeoCoordinate distribution = new GeoCoordinate(-25.746020, 28.187120);
+            return gpsLocation.GetDistanceTo(distribution);
+        }
+
+        public void GetCurrentGPSLocation()
+        {
+            Notify notify = new Notify();
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+            GeoCoordinate coord = watcher.Position.Location;
+            if (coord.IsUnknown != true)
+            {
+                gpsLocation = coord;
+            }
+            else
+            {
+                notify.warning("Failed to retrieve GPS location");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Long : " + gpsLocation.Longitude + " Lat : " + gpsLocation.Latitude;
+        }
+
+    }
+}
